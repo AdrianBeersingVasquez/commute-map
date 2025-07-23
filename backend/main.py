369b4254687980658515d808b2f8ff1d21e82c55
AdrianBeersingVasquez <57265495+AdrianBeersingVasquez@users.noreply.github.com>
2025-07-24 00:55:43 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,54 +1,17 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-import pandas as pd
+from fastapi import FastAPI, staticfiles
+from fastapi.responses import HTMLResponse
 import json
-import os
 
 app = FastAPI()
 
-#app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", staticfiles.StaticFiles(directory="static"), name="static")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-with open("../data/cities.json", "r") as f:
-    cities = json.load(f)
-
-@app.get("/")
-async def root():
-    return {"message": "Commute Map API"}
+@app.get("/{city}_heatmap.html")
+async def get_heatmap(city: str):
+    with open(f"static/preprocessing/{city.lower()}_heatmap.html", "r") as f:
+        return HTMLResponse(content=f.read())
 
 @app.get("/cities")
 async def get_cities():
-    return cities
-
-@app.get("/city/{city}")
-async def get_city_data(city_name: str):
-    city = next((c for c in cities if c["name"].lower() == city_name.lower()), None)
-    if city not in cities:
-        return {"error": "City not found"}
-    
-    data_file = city["data_file"]
-    if data_file.endswith(".pkl"):
-        df = pd.read_pickle(data_file)
-        data = df.to_dict(orient="records")
-        
-    elif data_file.endswith(".json"):
-        with open(data_file, "r") as f:
-            data = json.load(f)
-
-    return {
-        "city": city["name"],
-        "center": city["center"]#,
-        #"data": data
-        }
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    with open("data/cities.json", "r") as f:
+        return {"cities": [city["name"].lower() for city in json.load(f)]}
