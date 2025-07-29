c6808dbafa3f7c7253700ba06b7fed96459b70e0
AdrianBeersingVasquez <57265495+AdrianBeersingVasquez@users.noreply.github.com>
2025-07-29 10:56:36 +0100

    Remove proxy from vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/static/preprocessing': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path,
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react()],
+    server: command === 'serve' ? {
+      proxy: {
+        '/static/preprocessing': {
+          target: 'http://localhost:8000',
+          changeOrigin: true,
+          rewrite: (path) => path,
+        },
       },
-    },
-  },
+    } : undefined, // No proxy in build mode
+  };
 });
